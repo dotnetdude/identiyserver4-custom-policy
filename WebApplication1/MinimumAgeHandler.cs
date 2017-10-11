bf906ea51db13a56ec7799edeee3f65fd13f9af9
dotnetdude <suresh4job@gmail.com>
2017-10-11 21:24:56 +0000

    inital commit

---
+++ WebApplication1/MinimumAgeHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class MinimumAgeHandler : AuthorizationHandler<MinimumAgeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAgeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "client_Age"))
+            {
+                // .NET 4.x -> return Task.FromResult(0);
+                return Task.CompletedTask;
+            }
+
+            var clientage = Convert.ToInt16(context.User.FindFirst(
+                c => c.Type == "client_Age").Value);
+
+            if (clientage >= requirement.MinimumAge)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+
+    public class MinimumAgeRequirement : IAuthorizationRequirement
+    {
+        public int MinimumAge { get; private set; }
+
+        public MinimumAgeRequirement(int minimumAge)
+        {
+            MinimumAge = minimumAge;
+        }
+    }
+}
---
+++ WebApplication3/Config.cs
@@ -0,0 +1,53 @@
+﻿
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace WebApplication3
+{
+    public class Config
+    {
+        // scopes define the API resources in your system
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" },
+                    Claims =  new List<Claim>{new Claim("Age", "75")},
+                },
+                new Client
+                {
+                    ClientId = "client2",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" },
+                    Claims =  new List<Claim>{new Claim("Age", "17")},
+                }
+            };
+        }
+    }
+}
